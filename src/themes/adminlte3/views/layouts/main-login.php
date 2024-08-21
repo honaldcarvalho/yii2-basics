640da141460cadc41c3ae1fc58a29d0f2a77a25e
Honald Carvalho <honaldcarvalho@gmail.com>
2024-08-21 16:50:08 -0300

    title correction

--- src/themes/adminlte3/views/layouts/main-login.php
+++ src/themes/adminlte3/views/layouts/main-login.php
@@ -23,7 +23,7 @@ $params = Params::get();
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= $params->tile; ?> | Log in</title>
+    <title><?= $params->title; ?> | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
