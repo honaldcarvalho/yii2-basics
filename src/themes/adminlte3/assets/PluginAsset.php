1245e4d6123a29c0767744c0949c44ec60902030
Honald Carvalho <honaldcarvalho@gmail.com>
2024-08-21 16:21:02 -0300

    corrections

--- src/themes/adminlte3/assets/PluginAsset.php
+++ src/themes/adminlte3/assets/PluginAsset.php
@@ -6,22 +6,31 @@ use yii\web\AssetBundle;
 class PluginAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/weebz/yii2-basics/src/themes/adminlte3/web/dist/plugins';
-
     public $depends = [
         'weebz\yii2basics\themes\adminlte3\assets\BaseAsset'
     ];
 
     public static $pluginMap = [
         'tinymce' => [
+            'css' => 'tinymce/content/default/content.min.css',
+            'js' => 'tinymce/content/default/content.js',
             'js' => 'tinymce/tinymce.min.js',
         ],
         'jquery-ui' => [
-            'css' => 'jquery-ui/jquery-ui.css',
-            'js' => 'jquery-ui/jquery-ui.js',
+            'css' => 'jquery-ui/jquery-ui.min.css',
+            'js' => 'jquery-ui/jquery-ui.min.js',
+        ],
+        'icheck-bootstrap' => [
+            'css' => ['icheck-bootstrap/icheck-bootstrap.css']
         ],
         'cropperjs' => [
             'css' => 'cropperjs/css/cropper.css',
             'js' => 'cropperjs/js/cropper.js',
+        ],
+        'chart.js' => [
+            'css' => 'chart.js/Chart.min.css',
+            'js' => 'chart.js/Chart.min.js',
+            'js' => 'chart.js/Chart.bundle.min.js',
         ],
         'select2' => [
             'css' => 'select2/css/select2.min.css',
@@ -37,9 +46,6 @@ class PluginAsset extends AssetBundle
         ],
         'fontawesome' => [
             'css' => 'fontawesome-free/css/all.min.css'
-        ],
-        'icheck-bootstrap' => [
-            'css' => ['icheck-bootstrap/icheck-bootstrap.css']
         ],
         'sweetalert2' => [
             'css' => 'sweetalert2-theme-bootstrap-4/bootstrap-4.min.css',
