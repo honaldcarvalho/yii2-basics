5711d81c201f34c2506f5027b5b3300807c571c2
honaldcarvalho <honaldcarvalho@gmail.com>
2025-02-03 18:28:50 -0300

    add LoadQuery

---
+++ src/commands/LoadQueryController.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+/**
+ * USAGE: php yii load-query/run -f \\weebz\\yii2basics\\migrations\\m241010_171508_always_run_migration
+ */
+class LoadQueryController extends Controller
+{
+    public $file_name;
+    
+    public function options($actionID)
+    {
+        return ['file_name'];
+    }
+    
+    public function optionAliases()
+    {
+        return ['f' => 'file_name'];
+    }
+
+    public function actionRun()
+    {
+        $sql = file_get_contents(__DIR__ . "{$this->file_name}");
+        $this->execute($sql);
+
+        echo "Query completed!\n";
+    }
+}
