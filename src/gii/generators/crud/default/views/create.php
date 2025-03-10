bb7f20b3b350744ebf36b9bd4f647e6b76c48d72
Honald Carvalho <honaldcarvalho@gmail.com>
2025-03-10 19:40:42 -0300

    fix

--- src/gii/generators/crud/default/views/create.php
+++ src/gii/generators/crud/default/views/create.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="card-body">
             <div class="row">
                 <div class="col-md-12">
-                    <?=$this->render('_form', [
+                    <?= "<?=" ?>$this->render('_form', [
                         'model' => $model
                     ]) ?>
                 </div>
