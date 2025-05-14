36f5add9491a865fa6fcd0877aa7bcab7a569fcd
Honald Carvalho <honaldcarvalho@gmail.com>
2025-05-14 14:31:50 -0300

    branding

--- src/themes/adminlte3/views/layouts/footer.php
+++ src/themes/adminlte3/views/layouts/footer.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; <?=date('Y')?> <a class="text-weebz" href="https://weebz.com.br">Weebz</a>.</strong>
+    <strong>Copyright &copy; <?=date('Y')?> <a class="text-weebz" href="https://croacworks.com.br">CroacWorks</a> | <a class="text-weebz" href="https://weebz.com.br"> Weebz.</a></strong>
     Todos os direitos reservado.
 </footer>
