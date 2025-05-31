bc79d095663e6a5d5bbe9ade85207560fe8a90b4
Honald Carvalho <honaldcarvalho@gmail.com>
2025-05-31 11:48:17 -0300

    translate label

--- src/components/gridview/ResponsiveDataColumn.php
+++ src/components/gridview/ResponsiveDataColumn.php
@@ -3,20 +3,35 @@
 namespace weebz\yii2basics\components\gridview;
 
 use Yii;
+use yii\bootstrap5\Html;
 use yii\grid\DataColumn;
 
 class ResponsiveDataColumn extends DataColumn
 {
     public $responsive = true;
 
+
     public function init()
     {
         parent::init();
 
         if (!isset($this->contentOptions['data-title']) && $this->responsive) {
-            $this->contentOptions['data-title'] = Yii::t('app',$this->label) ?: ucfirst($this->attribute);
+            $label =  Yii::t('app',$this->label);
+
+            // Tenta pegar do modelo, se possível
+            if (!$label && $this->grid && $this->attribute) {
+                $models = $this->grid->dataProvider->getModels();
+                if (!empty($models)) {
+                    $model = reset($models);
+                    $labels = $model->attributeLabels();
+                    $label = $labels[$this->attribute] ?? ucfirst($this->attribute);
+                }
+            }
+
+            $this->contentOptions['data-title'] = Html::encode($label);
         }
 
+        // Classe útil para CSS
         $this->contentOptions['class'] = ($this->contentOptions['class'] ?? '') . ' col-' . $this->attribute;
     }
 }
