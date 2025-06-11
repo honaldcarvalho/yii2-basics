ab12a0e07524efe28297f71add6d547cdf61b577
Honald Carvalho <honaldcarvalho@gmail.com>
2025-06-11 18:43:39 -0300

    fix

--- src/controllers/SourceMessageController.php
+++ src/controllers/SourceMessageController.php
@@ -110,19 +110,4 @@ class SourceMessageController extends AuthController
         return $this->redirect(['index']);
     }
 
-    /**
-     * Finds the SourceMessage model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param int $id ID
-     * @return SourceMessage the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = SourceMessage::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
-    }
 }
