49102b688d9e3ac598ba9e7743a1b9f3604e7479
Honald Carvalho <honaldcarvalo@gmail.com>
2025-07-12 14:12:47 -0300

    fix change picture

--- src/views/menu/sidebar.php
+++ src/views/menu/sidebar.php
@@ -42,7 +42,11 @@ if(!empty($params->file_id) && $params->file != null){
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image user-image">
-                <i class="fas fa-user-circle img-circle elevation-2" alt="User Image"></i>
+                <?php if(Yii::$app->user->file):?>
+                    <img class='brand-image img-circle elevation-2' src="<?= $app->user->file->urlThumb; ?>" style='width:32px; opacity: .8' />
+                <?php else:?>
+                        <i class="fas fa-user-circle img-circle elevation-2" alt="User Image"></i>
+                <?php endif;?>
             </div>
             <div class="info">
                 <?= yii\helpers\Html::a($name_user, ['/user/profile', 'id' =>Yii::$app->user->identity->id],["class"=>"d-block"]) ?><br>
