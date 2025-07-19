572d8c57f6af620efa3775244048ad4991ebd6e3
Honald Carvalho <honaldcarvalo@gmail.com>
2025-07-19 20:02:46 -0300

    fix

--- src/migrations/m250719_200000_alter_id_autoincrement_menus.php
+++ src/migrations/m250719_200000_alter_id_autoincrement_menus.php
@@ -9,22 +9,13 @@ class m250719_200000_alter_id_autoincrement_menus extends Migration
 {
     public function safeUp()
     {
-        // Remove PK antiga se necessário
-        $this->dropPrimaryKey('PRIMARY', 'menus');
-
-        // Altera o campo `id` para AUTO_INCREMENT
-        $this->alterColumn('menus', 'id', $this->primaryKey()->unsigned()->notNull());
-
-        // Recria a PK se necessário
-        $this->addPrimaryKey('PRIMARY', 'menus', 'id');
+        // Altera diretamente sem dropar PK
+        $this->alterColumn('menus', 'id', $this->integer()->notNull()->append('AUTO_INCREMENT'));
     }
 
     public function safeDown()
     {
-        // Reverte para inteiro simples
+        // Reverte para inteiro comum (sem auto_increment)
         $this->alterColumn('menus', 'id', $this->integer()->notNull());
-
-        // Garante que a PK permaneça
-        $this->addPrimaryKey('PRIMARY', 'menus', 'id');
     }
 }
