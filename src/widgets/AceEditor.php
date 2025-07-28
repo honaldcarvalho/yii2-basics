1124efb3728c7f534ddb7f6bae2d6599685d33b7
Honald Carvalho <honaldcarvalo@gmail.com>
2025-07-28 19:32:17 -0300

    add label

--- src/widgets/AceEditor.php
+++ src/widgets/AceEditor.php
@@ -17,45 +17,46 @@ class AceEditor extends InputWidget
     public $readOnly = false;  // somente leitura
     public $clientOptions = [];
 
-public function run()
-{
-    $id = $this->options['id'];
-    $view = $this->getView();
+    public function run()
+    {
+        $id = $this->options['id'];
+        $view = $this->getView();
 
-    PluginAsset::register($view)->add(['ace']); // assume que você incluiu o `ace.js`
+        PluginAsset::register($view)->add(['ace']); // assume que você incluiu o `ace.js`
 
-    $style = "width: 100%; height: {$this->height};";
+        $style = "width: 100%; height: {$this->height};";
 
-    // Campo hidden sincronizado com o editor
-    if ($this->hasModel()) {
-        $input = Html::activeHiddenInput($this->model, $this->attribute, ['id' => "{$id}_hidden"]);
-    } else {
-        $input = Html::hiddenInput($this->name, $this->value, ['id' => "{$id}_hidden"]);
+        // Renderiza label (legenda)
+        if ($this->hasModel()) {
+            echo Html::label($this->model->getAttributeLabel($this->attribute), null, ['for' => $id]);
+            $input = Html::activeHiddenInput($this->model, $this->attribute, ['id' => "{$id}_hidden"]);
+        } else {
+            echo Html::label($this->name, null, ['for' => $id]);
+            $input = Html::hiddenInput($this->name, $this->value, ['id' => "{$id}_hidden"]);
+        }
+
+        echo $input;
+        echo Html::tag('div', Html::encode($this->value), ['id' => $id, 'style' => $style]);
+
+        // Corrige o nome da variável JS
+        $varName = 'editor_' . str_replace(['-', '.'], '_', $id);
+
+        $mode = $this->mode;
+        $theme = $this->theme;
+        $readOnly = $this->readOnly ? 'true' : 'false';
+
+        $js = <<<JS
+        var {$varName} = ace.edit("{$id}");
+        {$varName}.setTheme("ace/theme/{$theme}");
+        {$varName}.session.setMode("ace/mode/{$mode}");
+        {$varName}.setReadOnly($readOnly);
+        {$varName}.session.setValue(document.getElementById("{$id}_hidden").value);
+
+        {$varName}.session.on('change', function(){
+            document.getElementById("{$id}_hidden").value = {$varName}.getValue();
+        });
+        JS;
+
+        $view->registerJs($js, View::POS_READY);
     }
-
-    echo $input;
-    echo Html::tag('div', Html::encode($this->value), ['id' => $id, 'style' => $style]);
-
-    // Corrigir o nome da variável JS
-    $varName = 'editor_' . str_replace(['-', '.'], '_', $id);
-
-    $mode = $this->mode;
-    $theme = $this->theme;
-    $readOnly = $this->readOnly ? 'true' : 'false';
-
-    $js = <<<JS
-    var {$varName} = ace.edit("{$id}");
-    {$varName}.setTheme("ace/theme/{$theme}");
-    {$varName}.session.setMode("ace/mode/{$mode}");
-    {$varName}.setReadOnly($readOnly);
-    {$varName}.session.setValue(document.getElementById("{$id}_hidden").value);
-
-    {$varName}.session.on('change', function(){
-        document.getElementById("{$id}_hidden").value = {$varName}.getValue();
-    });
-    JS;
-
-    $view->registerJs($js, View::POS_READY);
-}
-
 }
