34cb87caaed7515a636724b8bc40eff11e65a16a
Honald Carvalho <honaldcarvalo@gmail.com>
2026-01-14 17:51:20 -0300

    fix

--- src/models/YoutubeMedia.php
+++ src/models/YoutubeMedia.php
@@ -232,7 +232,7 @@ class YoutubeMedia extends ModelCommon
                                 try {
                                     $interval = new \DateInterval($durationIso);
                                     $seconds = ($interval->h * 3600) + ($interval->i * 60) + $interval->s;
-                                    if ($seconds <= 60) {
+                                    if ($seconds <= 30) {
                                         echo "Skipping Short: {$item->id} ({$seconds}s)\n";
                                         continue;
                                     }
