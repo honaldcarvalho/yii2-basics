5ccaafc148dd9beb5ab6c5dfd609c1e557d4e1c3
Honald Carvalho <honaldcarvalho@gmail.com>
2026-05-27 06:44:18 -0300

    fix

--- src/migrations/m260527_063648_add_i18n_columns_to_configurations_table.php
+++ src/migrations/m260527_063648_add_i18n_columns_to_configurations_table.php
@@ -12,8 +12,8 @@ class m260527_063648_add_i18n_columns_to_configurations_table extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('{{%configurations}}', 'i18n_api_url', $this->string(255)->null()->after('google_maps_api_key'));
-        $this->addColumn('{{%configurations}}', 'i18n_api_token', $this->text()->null()->after('i18n_api_url'));
+        $this->addColumn('{{%configurations}}', 'i18n_api_url', $this->string(255)->null());
+        $this->addColumn('{{%configurations}}', 'i18n_api_token', $this->text()->null());
     }
 
     /**
