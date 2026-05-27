515212a8ef4420ff7f359af1be1ba863c06692e4
Honald Carvalho <honaldcarvalho@gmail.com>
2026-05-27 06:55:17 -0300

    Add i18n menu item with proper attributes and default values

--- src/migrations/m260527_063714_add_i18n_menu_item.php
+++ src/migrations/m260527_063714_add_i18n_menu_item.php
@@ -14,11 +14,16 @@ class m260527_063714_add_i18n_menu_item extends Migration
     {
         $this->insert('{{%menus}}', [
             'label' => 'i18n',
-            'menu_id' => null,
+            'menu_id' => 1, // System Menu
             'icon' => 'language',
+            'icon_style' => 'fas',
             'url' => '/configuration/i18n',
+            'visible' => 'configuration;i18n',
+            'active' => 'configuration',
+            'path' => 'app/controllers',
             'order' => 99,
             'status' => 1,
+            'only_admin' => 1
         ]);
     }
 
